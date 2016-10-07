efa95f11db3b4ae14f23d39228b34539e09d525e
Evgeniy <mforall29@gmail.com>
2016-10-07 20:29:50 +0300

    Initial Commit

---
+++ MPass/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MPass
+//
+//  Created by Евгений on 07.10.16.
+//  Copyright © 2016 Evgeniy. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MPass/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MPass
+//
+//  Created by Евгений on 07.10.16.
+//  Copyright © 2016 Evgeniy. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
